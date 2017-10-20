387ebe8846db16e2a4a4df60e761375ad0dd5054
Luca Frediani <luca.frediani@uit.no>
2017-10-20 08:40:42 +0200

    haar cleamup

--- haar_wavelets.py
+++ haar_wavelets.py
@@ -1,9 +1,6 @@
 import numpy
 import matplotlib.pyplot as plt
 
-nPolynomials = 4
-max_scale = 5
-
 def scaling(x):
     if( x >= 0 and x < 1):
         val = 1.0
@@ -25,22 +22,24 @@ def wavelet_nl(n, l, x):
 
 if __name__ == '__main__':
     x = numpy.arange(0, 1.0001, 0.001);
-    w0 = numpy.array([wavelet_nl(3,0,x[i]) for i in range(x.size)])
-    w1 = numpy.array([wavelet_nl(3,1,x[i]) for i in range(x.size)])
-    w2 = numpy.array([wavelet_nl(3,2,x[i]) for i in range(x.size)])
-    w3 = numpy.array([wavelet_nl(3,3,x[i]) for i in range(x.size)])
-    w4 = numpy.array([wavelet_nl(3,4,x[i]) for i in range(x.size)])
-    w5 = numpy.array([wavelet_nl(3,5,x[i]) for i in range(x.size)])
-    w6 = numpy.array([wavelet_nl(3,6,x[i]) for i in range(x.size)])
-    w7 = numpy.array([wavelet_nl(3,7,x[i]) for i in range(x.size)])
-    plt.plot(x, w0)
-    plt.plot(x, w1)
-    plt.plot(x, w2)
-    plt.plot(x, w3)
-    plt.plot(x, w4)
-    plt.plot(x, w5)
-    plt.plot(x, w6)
-    plt.plot(x, w7)
+    w00 = numpy.array([wavelet_nl(0,0,x[i]) for i in range(x.size)])
+    w10 = numpy.array([wavelet_nl(1,0,x[i]) for i in range(x.size)])
+    w11 = numpy.array([wavelet_nl(1,1,x[i]) for i in range(x.size)])
+    w20 = numpy.array([wavelet_nl(2,0,x[i]) for i in range(x.size)])
+    w21 = numpy.array([wavelet_nl(2,1,x[i]) for i in range(x.size)])
+    w22 = numpy.array([wavelet_nl(2,2,x[i]) for i in range(x.size)])
+    w23 = numpy.array([wavelet_nl(2,3,x[i]) for i in range(x.size)])
+    w30 = numpy.array([wavelet_nl(3,0,x[i]) for i in range(x.size)])
+    w31 = numpy.array([wavelet_nl(3,1,x[i]) for i in range(x.size)])
+    w32 = numpy.array([wavelet_nl(3,2,x[i]) for i in range(x.size)])
+    w33 = numpy.array([wavelet_nl(3,3,x[i]) for i in range(x.size)])
+    w34 = numpy.array([wavelet_nl(3,4,x[i]) for i in range(x.size)])
+    w35 = numpy.array([wavelet_nl(3,5,x[i]) for i in range(x.size)])
+    w36 = numpy.array([wavelet_nl(3,6,x[i]) for i in range(x.size)])
+    w37 = numpy.array([wavelet_nl(3,7,x[i]) for i in range(x.size)])
+    plt.plot(x, w10)
+    plt.plot(x, w22)
+    plt.plot(x, w36)
     plt.show()
 
 
