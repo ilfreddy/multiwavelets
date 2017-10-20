7295861f8c8c07c54f717dccd44bd373a148341f
Luca Frediani <luca.frediani@uit.no>
2017-10-20 13:16:12 +0200

    Solution from the students (fixed)

--- haar_wavelets.py
+++ haar_wavelets.py
@@ -2,14 +2,14 @@ import numpy
 import matplotlib.pyplot as plt
 
 def scaling(x):
-    if( x >= 0 and x < 1):
+    if( x >= 0.0 and x < 1.0):
         val = 1.0
     else:
         val = 0.0
     return val
 
 def wavelet(x):
-    return scaling(2*x) - scaling(2*x-1)
+    return (scaling_nl(1,0,x) - scaling_nl(1,1,x)) / numpy.sqrt(2.0)
 
     
 def scaling_nl(n, l, x):
@@ -21,25 +21,9 @@ def wavelet_nl(n, l, x):
     return numpy.sqrt(factor) * wavelet(factor*x-l)
 
 if __name__ == '__main__':
-    x = numpy.arange(0, 1.0001, 0.001);
-    w00 = numpy.array([wavelet_nl(0,0,x[i]) for i in range(x.size)])
-    w10 = numpy.array([wavelet_nl(1,0,x[i]) for i in range(x.size)])
-    w11 = numpy.array([wavelet_nl(1,1,x[i]) for i in range(x.size)])
-    w20 = numpy.array([wavelet_nl(2,0,x[i]) for i in range(x.size)])
-    w21 = numpy.array([wavelet_nl(2,1,x[i]) for i in range(x.size)])
-    w22 = numpy.array([wavelet_nl(2,2,x[i]) for i in range(x.size)])
-    w23 = numpy.array([wavelet_nl(2,3,x[i]) for i in range(x.size)])
-    w30 = numpy.array([wavelet_nl(3,0,x[i]) for i in range(x.size)])
-    w31 = numpy.array([wavelet_nl(3,1,x[i]) for i in range(x.size)])
-    w32 = numpy.array([wavelet_nl(3,2,x[i]) for i in range(x.size)])
-    w33 = numpy.array([wavelet_nl(3,3,x[i]) for i in range(x.size)])
-    w34 = numpy.array([wavelet_nl(3,4,x[i]) for i in range(x.size)])
-    w35 = numpy.array([wavelet_nl(3,5,x[i]) for i in range(x.size)])
-    w36 = numpy.array([wavelet_nl(3,6,x[i]) for i in range(x.size)])
-    w37 = numpy.array([wavelet_nl(3,7,x[i]) for i in range(x.size)])
-    plt.plot(x, w10)
-    plt.plot(x, w22)
-    plt.plot(x, w36)
+    x = numpy.arange(-0.1, 1.100001, 0.001);
+    y = numpy.array([wavelet_nl(2,2,x[i]) for i in range(x.size)])
+    plt.plot(x, y)
     plt.show()
 
 
--- multiwavelet_exercise.py
+++ multiwavelet_exercise.py
@@ -2,6 +2,8 @@ import numpy, sys, struct
 from math import sin, pi
 import matplotlib.pyplot as plt
 
+# luca.frediani@uit.no
+
 nPolynomials = 4
 max_scale = 5
 
@@ -9,15 +11,35 @@ max_scale = 5
 # Given an index j and a position x, returns the value of the j-th scaling function in x
 # Tips: use the legendre polynomial defined by numpy
 #
-def scaling(j,x):
-    print "FIXME"
+def scaling(j, x):
+    coef = []
+    for n in range(0, j+1):
+        if(n == j):
+            coef.append(1)
+        else:
+            coef.append(0)
+    factor = numpy.sqrt(2*j+1)
+    if(x >= 0 and x < 1):
+        polynom = numpy.polynomial.legendre.Legendre(coef, domain=[0,1])
+        value = polynom(x)
+    else:
+        value = 0.0
+    return factor * value
 
 #
 # Given the index j, the number of polynomials np, the position x,
 # returns the value of the j-th wavelet function of order np in x
 #
 def wavelet(j,np,x):
-    print "FIXME"
+    Sum = 0
+    G0 = getfilter("G", 0, np)
+    G1 = getfilter("G", 1, np)
+#    print G0
+#    print G1
+    for i in range(np):
+        wavelet_formel = (G0[j, i]*scaling_nl(i, 1, 0, x))/numpy.sqrt(2) - (G1[j, i]*scaling_nl(i, 1, 1, x))/numpy.sqrt(2)
+        Sum += wavelet_formel
+    return Sum
 
 #
 # Given the number of polynomials np and the position x,
@@ -38,14 +60,15 @@ def wavelets_nl(n,l,np,x):
 # returns the value of the scaling function j at scale n and translation l
 #
 def scaling_nl(j, n, l, x):
-    print "FIXME"
-
+    factor = 2**n
+    return numpy.sqrt(factor) * scaling(j,factor*x-l)
 #
 # Given the index j, the number of polynomials np, the scale n, the translation l and the position x,
 # returns the value of the j-th wavelet function of order np in x at scale n and translation l
 #
 def wavelet_nl(j, n, l, np, x):
-    print "FIXME"
+#    return 2**(n/2)*wavelet(j,np,x)*((2**n)*x-l)
+        return 2**(n/2)*wavelet(j,np,(2**n)*x-l)
 
 #
 # Load the requested filter matrix (H/G and 0/1) for a given order np
@@ -79,23 +102,30 @@ def getfilter(type="H",flag=0,np=4):
 
 
 if __name__ == '__main__':
-    x = numpy.arange(0, 1.0001, 0.001);
-    w0 = numpy.array([wavelet_nl(0,3,0,8,x[i]) for i in range(x.size)])
-    w1 = numpy.array([wavelet_nl(1,3,1,8,x[i]) for i in range(x.size)])
-    w2 = numpy.array([wavelet_nl(2,3,2,8,x[i]) for i in range(x.size)])
-    w3 = numpy.array([wavelet_nl(3,3,3,8,x[i]) for i in range(x.size)])
-    w4 = numpy.array([wavelet_nl(4,3,4,8,x[i]) for i in range(x.size)])
-    w5 = numpy.array([wavelet_nl(5,3,5,8,x[i]) for i in range(x.size)])
-    w6 = numpy.array([wavelet_nl(6,3,6,8,x[i]) for i in range(x.size)])
-    w7 = numpy.array([wavelet_nl(7,3,7,8,x[i]) for i in range(x.size)])
-    plt.plot(x, w0)
-    plt.plot(x, w1)
-    plt.plot(x, w2)
-    plt.plot(x, w3)
-    plt.plot(x, w4)
-    plt.plot(x, w5)
-    plt.plot(x, w6)
-    plt.plot(x, w7)
+    x = numpy.arange(-0.4, 1.4001, 0.001);
+    y0 = numpy.array([wavelet_nl(0,2,0,4,x[i]) for i in range(x.size)])
+    y1 = numpy.array([wavelet_nl(1,2,1,4,x[i]) for i in range(x.size)])
+    y2 = numpy.array([wavelet_nl(2,2,2,4,x[i]) for i in range(x.size)])
+    y3 = numpy.array([wavelet_nl(3,2,3,4,x[i]) for i in range(x.size)])
+#    w0 = numpy.array([wavelet_nl(0,3,0,8,x[i]) for i in range(x.size)])
+#    w1 = numpy.array([wavelet_nl(1,3,1,8,x[i]) for i in range(x.size)])
+#    w2 = numpy.array([wavelet_nl(2,3,2,8,x[i]) for i in range(x.size)])
+#    w3 = numpy.array([wavelet_nl(3,3,3,8,x[i]) for i in range(x.size)])
+#    w4 = numpy.array([wavelet_nl(4,3,4,8,x[i]) for i in range(x.size)])
+#    w5 = numpy.array([wavelet_nl(5,3,5,8,x[i]) for i in range(x.size)])
+#    w6 = numpy.array([wavelet_nl(6,3,6,8,x[i]) for i in range(x.size)])
+#    w7 = numpy.array([wavelet_nl(7,3,7,8,x[i]) for i in range(x.size)])
+#    plt.plot(x, w0)
+#    plt.plot(x, w1)
+#    plt.plot(x, w2)
+#    plt.plot(x, w3)
+#    plt.plot(x, w4)
+#    plt.plot(x, w5)
+#    plt.plot(x, w6)
+    plt.plot(x, y0)
+    plt.plot(x, y1)
+    plt.plot(x, y2)
+    plt.plot(x, y3)
     plt.show()
 
 
